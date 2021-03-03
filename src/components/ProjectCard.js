50750e1c6af754f7d7c94ef0fa20b83266eeefea
Rachael McIlhagga <rachael.mcilhagga@live.co.uk>
2021-03-03 16:33:34 -0500

    seperating icon componenets from hard code

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./styles.css";
-import { FaHtml5 } from "react-icons/fa"
-import { FaNodeJs } from "react-icons/fa"
-import { SiTravisci } from "react-icons/si"
-import { SiHeroku } from "react-icons/si"
-import { SiMysql } from "react-icons/si"
-import { SiJest } from "react-icons/si"
+// import { FaHtml5 } from "react-icons/fa"
+// import { FaNodeJs } from "react-icons/fa"
+// import { SiTravisci } from "react-icons/si"
+// import { SiHeroku } from "react-icons/si"
+// import { SiMysql } from "react-icons/si"
+// import { SiJest } from "react-icons/si"
 
 
 
-function ProjectCard() {
+function ProjectCard({title, description, image, gitLink, siteLink}) {
     return (
         <div className="container card-container ">
             <div className="flex-row card d-flex">
@@ -21,21 +21,21 @@ function ProjectCard() {
                     </div>
                 </div>
                 <div className="project-details col-md-12 col-lg-7 text-center">
-                    <h1 className="mt-4">Project One</h1>
-                    <p className="project-text mt-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <h1 className="mt-4 projectTitle">{title}</h1>
+                    <p className="project-text mt-3 projectDescription">{description}</p>
 
                     {/* Container for logo icons of technologies and frameworks used */}
                     <div class="d-flex justify-content-md-around align-items-center mb-3 logoContainer ">
-                        <FaHtml5 className="icons" />
+                        {/* <FaHtml5 className="icons" />
                         <FaNodeJs className="icons" />
                         <SiHeroku className="icons" />
                         <SiTravisci className="icons" />
                         <SiMysql className="icons" />
-                        <SiJest className="icons" />
+                        <SiJest className="icons" /> */}
                     </div>
 
-                    <button className="m-3 btn">Github</button>
-                    <button className="m-3 btn">Live Site</button>
+                    <a className="gitLink" href={gitLink} target="_blank" rel="noopener noreferrer"><button className="m-3 btn">Github</button></a>
+                    <a className="siteLink" href={siteLink} target="_blank" rel="noopener noreferrer"><button className="m-3 btn">Live Site</button></a>
 
                 </div>
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,14 +3,28 @@ import IntroBlobs from "../../components/Blobs/IntroBlobs"
 import ProjectCard from "../../components/ProjectCard"
 import AboutMe from "../../components/AboutMe"
 import "../../components/styles.css"
-
+// import LetsGo from "../../images/lets-go-img.png"
+import FaHtml5  from "react-icons/fa"
+import { FaNodeJs } from "react-icons/fa"
+import { SiTravisci } from "react-icons/si"
+import { SiHeroku } from "react-icons/si"
+import { SiMysql } from "react-icons/si"
+import { SiJest } from "react-icons/si"
 
 
 function Home() {
     return (<div>
     <IntroBlobs />
     <AboutMe />
-    <ProjectCard />
+    <ProjectCard 
+    title=" 'Let's Go' Travel Planner" 
+    description="'Let's Go' Travel Planner is a convenient planner that allows you to plan your future vacations, work trips, weekends away with just a few clicks. By pulling from both a well revered restaruant review API and expidition API you can save must-visit landmarks, experiences and restaurants. Since we implemented passport Authentication on visitng the site, When you wish to review your selections you can access your own personal itinerary where you'll find your choices grouped by destination. 
+    
+    In this applicaiton I was tasked with setting up the database to allow for the app to operate as a full CRUD application and applying the CSS design to the page."
+    // image={LetsGo}
+    gitLink="https://github.com/Mcilhaggis/lets-go-travel-planner"
+    siteLink="https://stormy-springs-60842.herokuapp.com/"
+    />
     <ProjectCard />
     <ProjectCard />
     <ProjectCard />
