d8b6b14e71759cdb59a8cb5d3af6a80150439976
Rachael McIlhagga <rachael.mcilhagga@live.co.uk>
2021-03-02 22:09:10 -0500

    adding basic componenents and styling

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./styles.css";
+import {FaHtml5} from "react-icons/fa"
+import {FaNodeJs} from "react-icons/fa"
+import {SiTravisci} from "react-icons/si"
+import {SiHeroku} from "react-icons/si"
+import {SiMysql} from "react-icons/si"
+import {SiJest} from "react-icons/si"
+
+
+
+function ProjectCard() {
+    return (
+        <div className="container card-container ">
+            <div className="row card">
+                <div className="project-details">
+                    <h1 className="col-md-12 col-lg-7 text-center mt-3">Project One</h1>
+                    <p className="project-text  col-md-12 col-lg-7 d-inline-block text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    
+                    {/* Container for logo icons of technologies and frameowrks used */}
+                    <div className="logoContainer col-md-12 col-lg-7 text-center d-flex justify-content-between">
+                        <FaHtml5 className="app-logo"/>
+                        <FaNodeJs className="app-logo"/>
+                        <SiHeroku className="app-logo"/>
+                        <SiTravisci className="app-logo"/>
+                        <SiMysql className="app-logo"/>
+                        <SiJest className="app-logo"/>
+                    </div>
+                    
+                    <div className="project-links text-center m-4">
+                        <button className="m-2 btn">Github</button>
+                        <button className="m-2 btn">Live Site</button>
+                    </div>
+
+                    <div className="project-image-container text-center float-right col-md-12 col-lg-5">
+                        <div className="blob-project">
+                            <div className="blob-project-inner">
+                                <img className="projectImage img-responsive mb-5" src="http://placehold.it/350" alt=""></img>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default ProjectCard;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import IntroBlobs from "../../components/Blobs/IntroBlobs"
+import ProjectCard from "../../components/ProjectCard"
+import "../../components/styles.css"
+
+
+
+function Home() {
+    return (<div>
+    <IntroBlobs />
+    <ProjectCard />
+    <ProjectCard />
+    </div>
+    )
+}
+
+export default Home; 
